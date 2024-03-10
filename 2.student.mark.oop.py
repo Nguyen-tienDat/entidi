44f22ca59b3067de9a85a96962b003fb2c00286e
Nguyen Tien Dat <datnt.22bi13079@usth.edu.vn>
2024-03-10 12:27:18 +0700

    Practical 2

---
+++ 2.student.mark.oop.py
@@ -0,0 +1,156 @@
+class Base:
+    def __init__(self):
+        self.id = 0
+        self.name = ''
+        self.dob = ''
+
+    def _get_name(self):
+        return self.name
+
+    def _set_name(self, name):
+        self.name = name
+
+    def _get_id(self):
+        return self.id
+
+    def _set_id(self, id):
+        self.id = id
+
+    def _get_dob(self):
+        return self.dob
+
+    def _set_dob(self, dob):
+        self.dob = dob
+
+
+class Student(Base):
+    student_list = []
+
+    def __init__(self, student_id, student_name, student_dob):
+        super().__init__()
+        self.id = student_id
+        self.name = student_name
+        self.dob = student_dob
+        Student.student_list.append(self)
+
+    def student_Info(self):
+        return f"StudentID: {self.id}, Student Name: {self.name}, Student DoB: {self.dob}"
+
+    @classmethod
+    def student_Add(cls):
+        student_id = input("Enter the ID of Student: ")
+        student_name = input("Enter the Name of Student: ")
+        student_dob = input("Enter the DoB of Student: ")
+        new_student = cls(student_id, student_name, student_dob)
+        cls.student_list.append(new_student)
+
+    @classmethod
+    def student_List(cls):
+        for student in cls.student_list:
+            print(student.student_Info())
+
+
+class Course(Base):
+    course_list = []
+
+    def __init__(self, course_id, course_name):
+        super().__init__()
+        self.id = course_id
+        self.name = course_name
+        Course.course_list.append(self)
+
+    def course_Info(self):
+        return f"CourseID: {self.id}, Course Name: {self.name}"
+
+    @classmethod
+    def course_Add(cls):
+        course_id = input("Enter the ID of course: ")
+        course_name = input("Enter the Name of course: ")
+        course = cls(course_id, course_name)
+        cls.course_list.append(course)
+
+    @classmethod
+    def list_courses(cls):
+        for course in cls.course_list:
+            print(course.course_Info())
+
+
+class Mark(Base):
+    marks = []
+
+    def __init__(self, course, student, mark):
+        super().__init__()
+        self.course = course
+        self.student = student
+        self.mark = mark
+
+    def mark_Info(self):
+        return f"StudentID: {self.student.id}, Student Name: {self.student.name}, CourseID: {self.course.id}, Course Name: {self.course.name}, Mark: {self.mark}"
+
+
+class System:
+    def __init__(self):
+        self.marks = []
+
+    def add_Mark(self):
+        course_id = int(input("Enter course ID: "))
+        student_id = int(input("Enter student ID: "))
+
+        course = None
+        for c in Course.course_list:
+            if c.id == course_id:
+                course = c
+                break
+        if not course:
+            print("Invalid course id")
+            return
+
+        student = None
+        for s in Student.student_list:
+            if s.id == student_id:
+                student = s
+                break
+        if not student:
+            print("Invalid student id")
+            return
+
+        mark = float(input("Enter the mark: "))
+        m = Mark(course, student, mark)
+        self.marks.append(m)
+        print("Mark added Successfully!")
+
+
+if __name__ == '__main__':
+    system = System()
+
+    while True:
+        print("_______Student Management System_______\n")
+        print("1. ADD INFORMATION OF STUDENT")
+        print("2. ADD INFORMATION OF COURSE")
+        print("3. ADD MARK OF STUDENT")
+        print("4. STUDENT'S LIST")
+        print("5. COURSE'S LIST")
+        print("6. MARK'S LIST")
+        print("7. EXIT!")
+
+        choice = int(input("Enter the option: "))
+
+        if choice == 1:
+            Student.student_Add()
+        elif choice == 2:
+            Course.course_Add()
+        elif choice == 3:
+            system.add_Mark()
+        elif choice == 4:
+            Student.student_List()
+        elif choice == 5:
+            Course.list_courses()
+        elif choice == 6:
+            for mark in system.marks:
+                print(mark.mark_Info())
+        elif choice == 7:
+            break
+        else:
+            print("Please enter a valid option.")
+
+        print("___Update Completed____")
